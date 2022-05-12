e8e40773262a84d86117b0ddea4a066d904f7f8f
Ashar Islam <55947821+asharislam@users.noreply.github.com>
2022-05-12 23:20:09 +0600

    Add files via upload

---
+++ function.py
@@ -0,0 +1,10 @@
+"""def welcome(person):
+    print(f"Hello {person.upper()}., welcome to python course.")
+
+print(welcome("ashar"))"""
+
+def add(a, b):
+    total = a + b
+    return total
+
+print(add(12, 8))
